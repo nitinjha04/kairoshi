cf78da1f2ef508883cfca84af6dfa34ad7c81567
nitinjha04 <nitinjha890@gmail.com>
2024-08-05 01:57:00 +0530

    readme

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -87,7 +87,7 @@ const Footer = () => {
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © {new Date().getFullYear()}{" "}
             <Link href="/">
-              <a className=" hover:underline">Tadashi™</a>
+              <a className=" hover:underline">Kairoshi</a>
             </Link>
             . All Rights Reserved. Developed by{" "}
             <a
